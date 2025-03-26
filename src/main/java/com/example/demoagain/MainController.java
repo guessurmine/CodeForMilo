ca067e53caa8a1c51fd214ab12d2301c15ff8230
guessurmine <trjstewie@outlook.com>
2025-03-26 10:39:59 +0800

    ohlala

---
+++ src/main/java/com/example/demoagain/MainController.java
@@ -0,0 +1,49 @@
+﻿package com.example.demoagain;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+
+public class MainController {
+    @FXML
+    private ListView<Contact> contactsListView;
+    private IContactDAO contactDAO;
+    public MainController() {
+        contactDAO = new MockContactDAO();
+    }
+
+    /**
+     * Renders a cell in the contacts list view by setting the text to the contact's full name.
+     * @param contactListView The list view to render the cell for.
+     * @return The rendered cell.
+     */
+    private ListCell<Contact> renderCell(ListView<Contact> contactListView) {
+        return new ListCell<>() {
+            /**
+             * Updates the item in the cell by setting the text to the contact's full name.
+             * @param contact The contact to update the cell with.
+             * @param empty Whether the cell is empty.
+             */
+            @Override
+            protected void updateItem(Contact contact, boolean empty) {
+                super.updateItem(contact, empty);
+                // If the cell is empty, set the text to null, otherwise set it to the contact's full name
+                if (empty || contact == null || contact.getFullName() == null) {
+                    setText(null);
+                } else {
+                    setText(contact.getFullName());
+                }
+            }
+        };
+    }
+
+    private void syncContacts() {
+        contactsListView.getItems().clear();
+        contactsListView.getItems().addAll(contactDAO.getAllContacts());
+    }
+    @FXML
+    public void initialize() {
+        contactsListView.setCellFactory(this::renderCell);
+        syncContacts();
+    }
+}
